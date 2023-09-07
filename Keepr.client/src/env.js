95667cc692b05b5ee412803f73caae68b83a9618
Jordan Diaz <jordanldiaz29@gmail.com>
2023-09-07 19:20:49 +0000

    update

--- Keepr.client/src/env.js
+++ Keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = 'https://localhost:7045'
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-wra2r4u13787lot5.us.auth0.com'
 export const clientId = 'Wcq0CtKAqonSFxcd1VwYcURAxkuwcs8t'
--- Keepr/Startup.cs
+++ Keepr/Startup.cs
@@ -96,7 +96,7 @@ public class Startup
       app.UseCors("CorsDevPolicy");
     }
 
-    app.UseHttpsRedirection();
+//    app.UseHttpsRedirection();
 
     app.UseDefaultFiles();
     app.UseStaticFiles();
